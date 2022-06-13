014c5919d3a26ebc873ff1f614a993947fa35a87
caleb-freitas <calebfreitas@tutanota.com>
2022-06-13 16:10:16 -0300

    feat: added list all enrollments query

---
+++ classroom/src/http/graphql/resolvers/enrollments.ts
@@ -0,0 +1,17 @@
+import { UseGuards } from "@nestjs/common";
+import { Query, Resolver } from "@nestjs/graphql";
+import { EnrollmentsService } from "../../../services/enrollments.service";
+import { AuthorizationGuard } from "../../auth/authorization.guard";
+import { Course } from "../models/course";
+import { Enrollment } from "../models/enrollment";
+
+@Resolver(() => Enrollment)
+export class EnrollmentsResolver {
+  constructor(private enrollmentsService: EnrollmentsService) {}
+
+  @Query(() => [Enrollment])
+  @UseGuards(AuthorizationGuard)
+  enrollments() {
+    return this.enrollmentsService.listAllEnrollments()
+  }
+}
