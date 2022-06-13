8cb58c1de60caad06b44f0e01b8d65e12b71eacb
caleb-freitas <calebfreitas@tutanota.com>
2022-06-13 16:00:09 -0300

    feat: added list all students query

--- classroom/src/http/http.module.ts
+++ classroom/src/http/http.module.ts
@@ -3,8 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { DatabaseModule } from '../database/database.module';
 import path from 'node:path';
-import { TestResolver } from './test.resolver';
 import { ApolloDriver } from '@nestjs/apollo';
+import { CoursesResolver } from './graphql/resolvers/courses';
+import { StudentsResolver } from './graphql/resolvers/students';
+import { EnrollmentsResolver } from './graphql/resolvers/enrollments';
+import { CoursesService } from '../services/courses.service';
+import { StudentsService } from '../services/students.service';
+import { EnrollmentsService } from '../services/enrollments.service';
 
 @Module({
   imports: [
@@ -15,6 +20,13 @@ import { ApolloDriver } from '@nestjs/apollo';
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
-  providers: [TestResolver],
+  providers: [
+    CoursesResolver,
+    StudentsResolver,
+    EnrollmentsResolver,
+    CoursesService,
+    StudentsService,
+    EnrollmentsService
+  ],
 })
 export class HttpModule {}
